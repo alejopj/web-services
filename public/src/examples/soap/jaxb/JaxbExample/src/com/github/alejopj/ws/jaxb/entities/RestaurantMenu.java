cb512e83436edfc69d48aaeaff4343c9b96328b6
Alejo Pacin Jul <alejopj@gmail.com>
2016-10-04 23:27:22 +0200

    Update JAXB example with the good approach

--- public/src/examples/soap/jaxb/JaxbExample/src/com/github/alejopj/ws/jaxb/entities/RestaurantMenu.java
+++ public/src/examples/soap/jaxb/JaxbExample/src/com/github/alejopj/ws/jaxb/entities/RestaurantMenu.java
@@ -1,19 +1,23 @@
 package com.github.alejopj.ws.jaxb.entities;
 
+import java.util.List;
+
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "restaurant_menu")
 public class RestaurantMenu {
 
-	private BreakfastMenu breakfastMenu;
+	private List<Food> breakfastMenu;
 
-	@XmlElement(name = "breakfast_menu")
-	public BreakfastMenu getBreakfastMenu() {
+	@XmlElementWrapper(name = "breakfast_menu")
+	@XmlElement(name = "food")
+	public List<Food> getBreakfastMenu() {
 		return breakfastMenu;
 	}
 
-	public void setBreakfastMenu(BreakfastMenu breakfastMenu) {
+	public void setBreakfastMenu(List<Food> breakfastMenu) {
 		this.breakfastMenu = breakfastMenu;
-	}	
+	}
 }
